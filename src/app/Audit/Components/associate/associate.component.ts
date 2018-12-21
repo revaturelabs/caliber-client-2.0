a57b4412157c6b974bbb1f7069de6c767fd7e428
pgerringer <44367095+pgerringer@users.noreply.github.com>
2018-12-21 15:22:27 -0500

    Feature audit table (#9)
    * Created the static audit page table using test data
    
    * Added click functionality to columns for dynamic updating from the front end to the backend

--- src/app/Audit/Components/associate/associate.component.ts
+++ src/app/Audit/Components/associate/associate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-associate',
@@ -7,9 +8,123 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssociateComponent implements OnInit {
 
-  constructor() { }
+  // List of test categories
+  categories = [
+    {
+      name: 'Java'
+    }
+  ];
 
-  ngOnInit() {
+  // List of test notes
+  notes = [
+    {
+      qcStatus: 'Undefined',
+      noteId: 0,
+      trainee: {
+        name: 'Forsberg, Justin',
+        flagNotes: '',
+        flagStatus: 'NONE'
+      }
+    },
+    {
+      qcStatus: 'Superstar',
+      noteId: 1,
+      trainee: {
+        name: 'Bill Boe',
+        flagNotes: '',
+        flagStatus: 'RED'
+      }
+    },
+    {
+      qcStatus: 'Good',
+      noteId: 2,
+      trainee: {
+        name: 'Forsberg, Justin',
+        flagNotes: '',
+        flagStatus: 'NONE'
+      }
+    },
+    {
+      qcStatus: 'Average',
+      noteId: 3,
+      trainee: {
+        name: 'Bill Boe',
+        flagNotes: '',
+        flagStatus: 'RED'
+      }
+    },
+    {
+      qcStatus: 'Poor',
+      noteId: 4,
+      trainee: {
+        name: 'Bill Boe',
+        flagNotes: '',
+        flagStatus: 'NONE'
+      }
+    }
+  ];
+
+  // Unimplemented functions
+  constructor() { }
+  ngOnInit() {}
+
+  // Change the trainee flag status
+  changeFlag(selectedNoteId: number): void {
+
+    // Loop through each note in notes until the target is found
+    for (let i = 0; i < this.notes.length; i++) {
+
+      // Find the clicked note
+      if (this.notes[i].noteId === selectedNoteId) {
+
+        // Determine the new status string
+        switch (this.notes[i].trainee.flagStatus) {
+          case 'NONE':
+          this.notes[i].trainee.flagStatus = 'RED';
+          break;
+
+          case 'RED':
+          this.notes[i].trainee.flagStatus = 'NONE';
+          break;
+        }
+      }
+    }
   }
 
+  // Change the Individual Feedback Status
+  changeIF(selectedNoteId: number): void {
+
+    // Loop through each note in notes until the target is found
+    for (let i = 0; i < this.notes.length; i++) {
+
+      // Find the clicked note
+      if (this.notes[i].noteId === selectedNoteId) {
+
+        // Create placeholder for new status string
+        let newStatus = '';
+
+        // Determine the new status string
+        switch (this.notes[i].qcStatus) {
+          case 'Undefined':
+            newStatus = 'Superstar';
+            break;
+          case 'Superstar':
+            newStatus = 'Good';
+            break;
+          case 'Good':
+            newStatus = 'Average';
+            break;
+          case 'Average':
+            newStatus = 'Poor';
+            break;
+          case 'Poor':
+            newStatus = 'Undefined';
+            break;
+        }
+
+        this.notes[i].qcStatus = newStatus;
+      }
+    }
+
+  }
 }

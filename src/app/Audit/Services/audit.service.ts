855e859015eb72f34e362e560cbfb9fb60563182
Ian Barney <ianbarney23@gmail.com>
2019-01-02 10:15:32 -0500

    saves added weeks

--- src/app/Audit/Services/audit.service.ts
+++ src/app/Audit/Services/audit.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Batch } from 'src/app/Batch/type/batch';
 import { Observable, Subject } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+/**
+ * sets headers for recieving JSON objects
+ */
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +20,7 @@ export class AuditService {
   url = 'http://localhost:9095';
   batchAllURL = '/vp/batch/all';
   batchesYearURL = '/vp/batch/';
+  batchUpdateURL = '/all/batch/update';
   yearsURL = '/all/batch/valid_years';
   selectedYear: number;
   selectedBatch: Batch;
@@ -24,6 +34,14 @@ export class AuditService {
 
   getAllYears(): Observable<number[]> {
     return this.http.get<number[]>(this.url + this.yearsURL);
+  }
+
+    /**
+   * updates specified batch in batch service
+   * @param batch batch to be updated
+   */
+  putBatch(batch: Batch): Observable<Batch> {
+    return this.http.put<Batch>(this.url + this.batchUpdateURL, batch, httpOptions);
   }
 
   

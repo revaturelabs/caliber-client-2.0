3a4c45a46f8db25dac767ad851ac07f84dcd6d6e
groman-gonzalez <gerald.romangonzalez@gmail.com>
2019-01-04 15:42:40 -0500

    Initial push to staging

--- src/app/Audit/types/Trainee.ts
+++
@@ -1,9 +0,0 @@
-export class Trainee {
-    name: String;
-    email: String;
-    batchId: Number;
-    flagStatus: String;
-    flagNotes: String;
-    flagAuthor: String;
-    flagTimestamp: Number;
-}
